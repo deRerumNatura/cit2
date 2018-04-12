a5b51c87934a5d79a82c1a65562e5ebdd2e5decd
dererumnatura <pollindrom@gmail.com>
2018-04-12 23:29:30 +0300

    with dir

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -3,8 +3,8 @@
           <h1 class="jumbotron-heading">Welcome, CodeIT !</h1>
           <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
           <p>
-            <a href="<?php echo FULL_PATH?>/singin" class="btn btn btn-info my-2">Sing in</a>
-            <a href="<?php echo FULL_PATH?>/singup" class="btn btn-secondary my-2">Sing up</a>
+            <a href="<?php echo FULL_PATH?>/singin" class="btn btn btn-info my-2">Sign in</a>
+            <a href="<?php echo FULL_PATH?>/singup" class="btn btn-secondary my-2">Sign up</a>
           </p>
         </div>
       </section>
